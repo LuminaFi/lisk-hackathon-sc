3ed12362c0bc3f449a5bc98041e5ac09d944b4c5
adamafriansyahb <adamafriansyahb@gmail.com>
2025-04-27 22:11:47 +0700

    feat: set current reserve to be the balance of IDRX in the smart contract's address

--- src/IDRXTransferManager.sol
+++ src/IDRXTransferManager.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 
 /**
  * @title IDRXTransferManager
@@ -24,7 +24,7 @@ contract IDRXTransferManager is AccessControl, ReentrancyGuard, Pausable {
     uint256 public minTransferAmount = 10000;
     uint256 public maxTransferAmount = 10000000;
 
-    uint256 public currentReserve;
+    // uint256 public currentReserve;
     uint256 public reserveThreshold = 1000000;
     uint256 public lowReserveMaxAmount = 100000;
 
@@ -82,24 +82,33 @@ contract IDRXTransferManager is AccessControl, ReentrancyGuard, Pausable {
         address _recipient,
         uint256 _idrxAmount
     ) external nonReentrant whenNotPaused onlyRole(OPERATOR_ROLE) {
+        // Validate recipient's address
         require(_recipient != address(0), "Invalid recipient");
+        
+        // Validate user transfer amount against minimum transfer amount
         require(
             _idrxAmount >= minTransferAmount,
             "Below minimum transfer amount"
         );
 
+        // Validate user transfer amount against maximum transfer amount
         uint256 effectiveMaxAmount = getEffectiveMaxTransferAmount();
         require(
             _idrxAmount <= effectiveMaxAmount,
             "Exceeds maximum transfer amount"
         );
+
+        // Validate current idrx reserve in SC wallet
+        uint256 currentReserve = getCurrentReserveAmount();
         require(_idrxAmount <= currentReserve, "Insufficient reserve");
 
         uint256 totalFee = calculateFee(_idrxAmount);
         uint256 recipientAmount = _idrxAmount - totalFee;
+        
+        // TODO: can be removed since reserveAmount will be automatically reduced as we call .transfer()
+        // currentReserve -= _idrxAmount;
 
-        currentReserve -= _idrxAmount;
-
+        // Send idrx to recipient
         require(
             IERC20(idrxToken).transfer(_recipient, recipientAmount),
             "IDRX transfer failed"
@@ -132,7 +141,10 @@ contract IDRXTransferManager is AccessControl, ReentrancyGuard, Pausable {
             "IDRX transfer failed"
         );
 
-        currentReserve += _amount;
+        // TODO: can be removed since reserve amount will be added as .transferFrom() gets executed successfully (?)
+        // currentReserve += _amount;
+
+        uint256 currentReserve = getCurrentReserveAmount();
 
         if (paused() && currentReserve >= emergencyThreshold) {
             _unpause();
@@ -152,9 +164,12 @@ contract IDRXTransferManager is AccessControl, ReentrancyGuard, Pausable {
     ) external nonReentrant onlyRole(ADMIN_ROLE) {
         require(_amount > 0, "Amount must be positive");
         require(_to != address(0), "Invalid recipient");
+
+        uint256 currentReserve = getCurrentReserveAmount();
         require(_amount <= currentReserve, "Insufficient reserve");
 
-        currentReserve -= _amount;
+        // TODO: can be removed since reserveAmount will be automatically reduced as we call .transfer()
+        // currentReserve -= _amount;
 
         require(
             IERC20(idrxToken).transfer(_to, _amount),
@@ -250,7 +265,7 @@ contract IDRXTransferManager is AccessControl, ReentrancyGuard, Pausable {
      */
     function unpause() external onlyRole(ADMIN_ROLE) {
         require(
-            currentReserve >= emergencyThreshold,
+            getCurrentReserveAmount() >= emergencyThreshold,
             "Reserve below emergency threshold"
         );
         _unpause();
@@ -272,9 +287,12 @@ contract IDRXTransferManager is AccessControl, ReentrancyGuard, Pausable {
      * @return The effective maximum amount
      */
     function getEffectiveMaxTransferAmount() public view returns (uint256) {
+        uint256 currentReserve = getCurrentReserveAmount();
+
         if (currentReserve < reserveThreshold) {
             return lowReserveMaxAmount;
         }
+
         return maxTransferAmount;
     }
 
@@ -289,26 +307,31 @@ contract IDRXTransferManager is AccessControl, ReentrancyGuard, Pausable {
         view
         returns (uint256 reserve, uint256 effectiveMaxAmount, bool isActive)
     {
-        return (currentReserve, getEffectiveMaxTransferAmount(), !paused());
+        return (getCurrentReserveAmount(), getEffectiveMaxTransferAmount(), !paused());
     }
 
-    /**
-     * @dev Recover any excess IDRX (beyond the tracked reserve)
-     * @param _to Address to send the excess IDRX to
-     */
-    function recoverExcessIDRX(
-        address _to
-    ) external nonReentrant onlyRole(ADMIN_ROLE) {
-        require(_to != address(0), "Invalid recipient");
+    // TODO: confirm about this function's use case
+    // /**
+    //  * @dev Recover any excess IDRX (beyond the tracked reserve)
+    //  * @param _to Address to send the excess IDRX to
+    //  */
+    // function recoverExcessIDRX(
+    //     address _to
+    // ) external nonReentrant onlyRole(ADMIN_ROLE) {
+    //     require(_to != address(0), "Invalid recipient");
 
-        uint256 balance = IERC20(idrxToken).balanceOf(address(this));
-        require(balance > currentReserve, "No excess IDRX to recover");
+    //     uint256 balance = IERC20(idrxToken).balanceOf(address(this));
+    //     require(balance > currentReserve, "No excess IDRX to recover");
 
-        uint256 excessAmount = balance - currentReserve;
+    //     uint256 excessAmount = balance - currentReserve;
 
-        require(
-            IERC20(idrxToken).transfer(_to, excessAmount),
-            "IDRX transfer failed"
-        );
+    //     require(
+    //         IERC20(idrxToken).transfer(_to, excessAmount),
+    //         "IDRX transfer failed"
+    //     );
+    // }
+
+    function getCurrentReserveAmount() public view returns (uint256) {
+        return IERC20(idrxToken).balanceOf(address(this));
     }
 }
